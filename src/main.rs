ba5bf7c009dd3b91d9760857917c7fef14a8e313
Dekoder-py <kyle@codingcorner.dev>
2026-03-20 10:03:38 +1300

    add structs for response, make request with api key from env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Resp {
+    devlogs: Vec<Devlog>
+}
+
+#[derive(Deserialize, Debug)]
+struct Devlog {
+    id: u32,
+    body: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = env::var("FT_API_KEY").expect("Failed to get API key from env");
+    let url = "https://flavortown.hackclub.com/api/v1/devlogs";
+    let client = reqwest::blocking::Client::new();
+    let resp: Resp = client.get(url).bearer_auth(token)
+        .send().expect("Failed to fetch")
+        .json().expect("Failed to parse");
+    
+    for devlog in resp.devlogs.iter() {
+        println!("{:?}", devlog);
+    }
 }
